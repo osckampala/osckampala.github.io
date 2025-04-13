04b173dbdcaf263123e070c949b3b9a31a8fb6be
Jude_Otine <judextine28@gmail.com>
2025-04-13 11:26:18 +0300

    Refactored a feature

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import Home from "@/pages/Home";
-import Events from "@/pages/Events";
-import EventDetail from "@/pages/EventDetail";
-import Projects from "@/pages/Projects";
-import ProjectDetail from "@/pages/ProjectDetail";
-import Blog from "@/pages/Blog";
-import BlogDetail from "@/pages/BlogDetail";
 import NotFound from "@/pages/NotFound";
 import BackToTop from "@/components/BackToTop";
 
@@ -31,12 +25,6 @@ const App = () => (
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/events/:slug" element={<EventDetail />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/projects/:slug" element={<ProjectDetail />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:slug" element={<BlogDetail />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
