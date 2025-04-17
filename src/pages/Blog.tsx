853f320a0ac0bd919e247a451b5f5e9ad722fa52
Jude_Otine <judextine28@gmail.com>
2025-04-17 15:20:06 +0300

    imported components and utils

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import SkeletonCard from "@/components/SkeletonCard";
+import { getMarkdownFiles, MarkdownFile } from "@/utils/markdownLoader";
+import BlogPost from "@/components/BlogPost";
+
+
+
+export default Blog;
