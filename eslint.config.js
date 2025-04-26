3a49b0157b5a8f283642561250be22be183046a8
Moses Musoke <musokemoses72@gmail.com>
2025-04-26 05:31:42 +0300

    Setup the workflow to run checks for linting and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,6 @@ export default [
         { allowConstantExport: true },
       ],
       "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-require-imports": "off",
